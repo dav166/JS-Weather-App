280536fa9951c82e1bf0077560bfaaeb99f6e8a1
David <5992264+dav166@users.noreply.github.com>
2024-02-26 14:53:11 -0600

    feat: Establish API Key and prep declarations for search function

---
+++ script.js
@@ -0,0 +1,12 @@
+const container = document.querySelector('.container');
+const search = document.querySelector('.search-box button');
+const weatherBox = document.querySelector('.weather-box');
+const weatherDetails = document.querySelector('.weather-details');
+const error404 = document.querySelector('.not-found');
+
+search.addEventListener('click', () => {
+    const APIKey = '728b0ee6df5687559812bd3169ad77b7';
+    const city = document.querySelector('.search-box input').value;
+
+
+})
