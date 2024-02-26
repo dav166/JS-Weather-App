c30130c451bd6dca7e82adb3feb0bd9da9107181
David <5992264+dav166@users.noreply.github.com>
2024-02-26 16:03:38 -0600

    fix: Squash bugs and update api key

--- script.js
+++ script.js
@@ -6,13 +6,13 @@ const error404 = document.querySelector('.not-found');
 
 search.addEventListener('click', () => {
 
-    const APIKey = '728b0ee6df5687559812bd3169ad77b7';
+    const APIKey = '25246c89240ddb28c9da9b3fced26257';
     const city = document.querySelector('.search-box input').value;
 
     if (city === '')
         return;
 
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIKey}`)
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}&units=imperial`)
         .then(response => response.json())
         .then(json => {
 
@@ -27,7 +27,7 @@ search.addEventListener('click', () => {
 
             error404.style.display = 'none';
             error404.classList.remove('fadeIn');
-
+ 
             const image = document.querySelector('.weather-box img');
             const temperature = document.querySelector('.weather-box .temperature');
             const description = document.querySelector('.weather-box .description');
@@ -59,10 +59,10 @@ search.addEventListener('click', () => {
                     image.src = '';
             }
 
-            temperature.innerHTML = `${parseInt(json.main.temp)}<span>°C</span>`;
+            temperature.innerHTML = `${parseInt(json.main.temp)}<span>°F</span>`;
             description.innerHTML = `${json.weather[0].description}`;
             humidity.innerHTML = `${json.main.humidity}%`;
-            wind.innerHTML = `${parseInt(json.wind.speed)}Km/h`;
+            wind.innerHTML = `${parseInt(json.wind.speed)}mph`;
 
             weatherBox.style.display = '';
             weatherDetails.style.display = '';
@@ -73,5 +73,5 @@ search.addEventListener('click', () => {
 
         });
 
-        
+
 });
